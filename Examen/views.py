48373b22221710a602601bc5602cf30d698e54aa
Bayron01 <31601396+Bayron01@users.noreply.github.com>
2018-07-08 23:35:33 -0400

    nuevo

--- Examen/views.py
+++ Examen/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from Examen.models import Contacto
 # Create your views here.
 def index(request):
 	data = {}
@@ -8,5 +8,12 @@ def index(request):
 
 def listar(request):
 	data = {}
+	data['object_list'] = Contacto.objects.all().order_by('id')
 	template_name = 'contactos/listar.html'
 	return render(request, template_name, data)
+
+def detalle(request, contactos_id):
+    data = {}
+    data['contactos'] = Contacto.objects.get(pk=contactos_id)
+    template_name = 'contactos/detalle.html'
+    return render(request, template_name, data)
